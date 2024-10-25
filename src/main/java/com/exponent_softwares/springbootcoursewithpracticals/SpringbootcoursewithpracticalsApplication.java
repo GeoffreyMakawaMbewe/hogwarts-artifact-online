da6d8e7cd33fcd01bc320e2fd3a78e7589717794
Geoffrey Makawa <geoffreymakawa04@gmail.com>
2024-10-25 15:32:53 +0200

    Starting new springboot + spring security course

---
+++ src/main/java/com/exponent_softwares/springbootcoursewithpracticals/SpringbootcoursewithpracticalsApplication.java
@@ -0,0 +1,13 @@
+package com.exponent_softwares.springbootcoursewithpracticals;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootcoursewithpracticalsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootcoursewithpracticalsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/exponent_softwares/springbootcoursewithpracticals/SpringbootcoursewithpracticalsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exponent_softwares.springbootcoursewithpracticals;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootcoursewithpracticalsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
